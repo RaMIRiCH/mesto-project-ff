900d173ac80d62a86dba1d87e9d294da1d9965b6
Artem Marin <RaMIRiCH09@yandex.ru>
2024-11-05 15:10:34 +0300

    Проект №5. V.1.0.

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,12 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const cardTemplate = document.querySelector('#card-template');
+function addCard(name, link, deleteCallback) {
+    const cardBlock = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardTitle = cardTemplate.querySelector('.card__title');
+    const cardImage = cardTemplate.querySelector('.card__image');
+    const deleteButton = cardTemplate.querySelector('.card__delete-button');
+    cardTitle.textContent = name;
+    cardImage.src = link;
+}
