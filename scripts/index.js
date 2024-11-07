578f0ba946bfbabbd6b1e1cae95cd25795ae135d
Artem Marin <RaMIRiCH09@yandex.ru>
2024-11-07 13:42:10 +0300

    Проект №5. V.1.4.

--- scripts/index.js
+++ scripts/index.js
@@ -4,22 +4,21 @@ const cardTemplate = document.querySelector('#card-template');
 const placecCardList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
 // @todo: Функция удаления карточки
-function addCard(name, link, deleteCallback) {
+function createCard(name, link, deleteCallback) {
     const cardBlock = cardTemplate.content.querySelector('.card').cloneNode(true);
     const cardTitle = cardBlock.querySelector('.card__title');
     const cardImage = cardBlock.querySelector('.card__image');
     const deleteButton = cardBlock.querySelector('.card__delete-button');
-    
     cardTitle.textContent = name;
     cardImage.src = link;
-    deleteButton.addEventListener('click', function () {
+    cardImage.alt = name;
+    deleteCallback = deleteButton.addEventListener('click', function () {
         cardBlock.remove();
-        if (deleteCallback) deleteCallback();
     });
     return cardBlock;
 };
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function (element) {
-    const cardBlock = addCard(element.name, element.link);
+    const cardBlock = createCard(element.name, element.link);
     placecCardList.append(cardBlock);
 });
